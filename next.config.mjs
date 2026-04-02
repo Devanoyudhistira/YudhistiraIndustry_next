7c3944da8e57c91b765ae07f7c652dd6df124eee
Devanoyudhistira <devanotira@gmail.com>
2026-04-02 23:09:18 +0700

    fix:config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
   images: {
     remotePatterns: [
       {
@@ -10,6 +10,17 @@ const nextConfig = {
         pathname: "/storage/v1/object/public/**",
       },
     ],
+  },
+
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/pdf": [
+        "./node_modules/@sparticuz/chromium/bin/**",
+      ],
+      "/app/api/pdf/route": [
+        "./node_modules/@sparticuz/chromium/bin/**",
+      ],
+    },
   },
 };
 
