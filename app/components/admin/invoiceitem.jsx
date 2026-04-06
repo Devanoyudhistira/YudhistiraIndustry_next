7cf400fb31d615138017d525e6d9e38058be781c
Devanoyudhistira <devanotira@gmail.com>
2026-04-06 19:37:43 +0700

    fix:name on invoice

--- app/components/admin/invoiceitem.jsx
+++ app/components/admin/invoiceitem.jsx
@@ -3,6 +3,8 @@ import truncate from "@/app/function/truncat";
 import { Cash } from "react-bootstrap-icons";
 
 export default function Invoiceitem({ namaitem, harga, status, tanggal, user }) {
+    const firstname = Array.from(user)[0]
+    const lastname =  user.split(" ")[1] ?  Array.from(user.split(" ")[1])[0] : ""
     const statusColor = (statusproduct) => {
         let style = ""
         switch (statusproduct) {
@@ -22,7 +24,7 @@ export default function Invoiceitem({ namaitem, harga, status, tanggal, user }) 
         <div className="w-[96%] -ml-5 self-center h-22 rounded-xl bg-zinc-50 shadow-[1px_1px_3px_black] border border-zinc-900 flex flex-col px-1 py-2 justify-between items-center" >
             <div className="flex px-2 justify-between items-center w-full" >
                 <div className="flex gap-2 justify-center items-center" >
-                    <h1 className="w-min h-min p-2 bg-purple-200 rounded-md uppercase font-semibold text-md " > {Array.from(user)[0] + Array.from(user.split(" ")[1])[0]}  </h1>
+                    <h1 className="w-10 h-10 flex items-center text-xl justify-center text-center bg-purple-200 rounded-md uppercase font-semibold text-md " > {firstname + lastname}  </h1>
                     <div className="flex flex-col items-start" >
                         <h3 className="font-semibold text-md" >{truncate(user, 18)}</h3>
                         <h4 className="font-semibold text-xs" >{tanggal}</h4>
